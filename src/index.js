e84b4e8fcacc2b4b0a48d8cf35bdb1c5566ad15e
Henry Hill <henry.jacob.hill@gmail.com>
2019-08-09 09:20:18 -0400

    Configured redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import configureStore from "./store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
 );
